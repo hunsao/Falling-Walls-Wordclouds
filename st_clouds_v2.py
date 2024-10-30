adcca037156b3eb197a0fff8ff965ed0b299ca1d
David C. <84016055+hunsao@users.noreply.github.com>
2024-10-30 11:27:39 +0100

    Update st_clouds_v2.py
    A v3

--- st_clouds_v2.py
+++ st_clouds_v2.py
@@ -26,6 +26,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload, HttpRequest
 from googleapiclient.errors import HttpError
 
+
 st.set_page_config(
     page_title="WordClouds",
     page_icon="🧠",
@@ -34,18 +35,14 @@ st.set_page_config(
 
 def get_google_services():
     try:
-        # Obtener la cadena codificada de la variable de entorno
         encoded_sa = os.getenv('GOOGLE_SERVICE_ACCOUNT')
         if not encoded_sa:
             raise ValueError("La variable de entorno GOOGLE_SERVICE_ACCOUNT no está configurada")
 
-        # Decodificar la cadena
         sa_json = base64.b64decode(encoded_sa).decode('utf-8')
 
-        # Crear un diccionario a partir de la cadena JSON
         sa_dict = json.loads(sa_json)
 
-        # Crear las credenciales
         credentials = service_account.Credentials.from_service_account_info(
             sa_dict,
             scopes=[
@@ -72,7 +69,6 @@ def load_data_from_sheet(spreadsheet_id, range_name, sheets_service):
             st.warning("No se encontraron datos en la hoja de cálculo.")
             return None
         else:
-            # Convertir los datos en un DataFrame
             data = pd.DataFrame(values[1:], columns=values[0])
             
             # Verificar que hay suficientes columnas y seleccionar solo G y H
@@ -88,102 +84,86 @@ def load_data_from_sheet(spreadsheet_id, range_name, sheets_service):
         st.error(f"Error al cargar datos de Google Sheets: {str(e)}")
         return None
 
-# def create_wordcloud(text):
-#     # wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
-#     wordcloud = WordCloud(
-#     width=800,
-#     height=400,
-#     background_color="white",
-#     contour_color='black',
-#     colormap="magma",
-#     stopwords=None,  
-#     ).generate(text)
-
-#     fig, ax = plt.subplots()
-#     ax.imshow(wordcloud, interpolation='bilinear')
-#     ax.axis('off')
-
-#     plt.figtext(0.5, 0.02, f"Wordcloud - ", ha="center", fontsize=11, color='grey')
-
-#     return fig
-
 def create_wordcloud(text, title):
     wordcloud = WordCloud(
-    width=800,
-    height=400,
-    background_color="white",
-    contour_color='black',
-    contour_width = 2,
-    colormap="RdBu_r",
-    stopwords=None,  
+        width=600,  # Ajustado para hacer los wordclouds más pequeños
+        height=300,  # Ajustado para hacer los wordclouds más pequeños
+        background_color="white",
+        contour_color='black',
+        contour_width=2,
+        colormap="RdBu_r",
+        stopwords=None,
     ).generate(text)
 
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(8, 4))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
-    plt.figtext(0.5, 0.01, title, ha="center", fontsize=11, color='grey')  # Actualizado para incluir el título
+    plt.figtext(0.5, 0.01, title, ha="center", fontsize=11, color='grey')
     plt.tight_layout()
-    
+
     return plt
+
+qr_image_path = Path(__file__).parent / "qr.png"
 
 def main():
     st.markdown("<h1 style='text-align: center;'>How is age depicted in Generative AI?</h1>", unsafe_allow_html=True)
     st.markdown("")
-    #st.markdown("<h5 style='text-align: center;'>text text text</h5>", unsafe_allow_html=True)
+    st.markdown("")
 
-    # Conectar a Google Drive y Sheets
     drive_service, sheets_service = get_google_services()
     spreadsheet_id = "1kkpKzDOkwJ58vgvp0IIAhS-yOSJxId8VJ4Bjxj7MmJk"
-    range_name = "Sheet1!A1:I"  # Ajusta el rango según tu hoja de cálculo
+    range_name = "Sheet1!A1:I"
 
-    # Configurar el tiempo de actualización
-    #refresh_interval = st.slider("Intervalo de actualización (segundos)", 10, 300, 60)
-    refresh_interval = 600
+    refresh_interval = 60
 
-    # Crear contenedores vacíos para cada grupo y cada tipo de gráfico
-    col1, col2 = st.columns(2)
+    col1, spacer, col2 = st.columns([1, 0.2, 1])
 
-    # Contenedores para el grupo "older"
-    older_tag_plot = col1.empty()
-    older_word_plot = col1.empty()
+    # Títulos de las columnas
+    col1.markdown("<h3 style='text-align: center;'>People</h3>", unsafe_allow_html=True)
+    col2.markdown("<h3 style='text-align: center;'>Older People</h3>", unsafe_allow_html=True)
 
-    # Contenedores para el grupo "neutral"
-    neutral_tag_plot = col2.empty()
-    neutral_word_plot = col2.empty()
+    # Contenedores para wordclouds
+    neutral_word_plot = col1.empty()
+    older_word_plot = col2.empty()
 
-    last_update_text = st.empty()  # Contenedor para la última actualización
+    last_update_text = st.empty()
 
     while True:
         # Cargar los datos
         data = load_data_from_sheet(spreadsheet_id, range_name, sheets_service)
         if data is not None:
-            # Filtrar los datos para los grupos
             older_data = data[data['Group'] == 'older']
             neutral_data = data[data['Group'] == 'neutral']
 
-            # Crear texto para los wordclouds
-            older_tags = " ".join(older_data["Tags"].dropna())
-            older_words = " ".join(older_data["Words"].dropna())
-            neutral_tags = " ".join(neutral_data["Tags"].dropna())
-            neutral_words = " ".join(neutral_data["Words"].dropna())
+            older_text = " ".join(older_data["Tags"].dropna()) + " " + " ".join(older_data["Words"].dropna())
+            neutral_text = " ".join(neutral_data["Tags"].dropna()) + " " + " ".join(neutral_data["Words"].dropna())
 
-            # Actualizar plots para el grupo "older"
-            with older_tag_plot:
-                older_tag_plot.pyplot(create_wordcloud(older_tags, "Tags - Older person"))  # Título actualizado
-            with older_word_plot:
-                older_word_plot.pyplot(create_wordcloud(older_words, "Words - Older person)"))  # Título actualizado
+           # Estilos CSS para ajustar el espaciado entre los gráficos
+            neutral_style = "<div style='margin-left: 20px; margin-right: -20px;'>"
+            older_style = "<div style='margin-left: -20px; margin-right: 20px;'>"
 
-            # Actualizar plots para el grupo "neutral"
-            with neutral_tag_plot:
-                neutral_tag_plot.pyplot(create_wordcloud(neutral_tags, "Tags - Person"))  # Título actualizado
             with neutral_word_plot:
-                neutral_word_plot.pyplot(create_wordcloud(neutral_words, "Tags - Person"))  # Título actualizado
+                neutral_word_plot.pyplot(create_wordcloud(neutral_text, ""))
+
+            with older_word_plot:
+                older_word_plot.pyplot(create_wordcloud(older_text, ""))
+
+            st.markdown("<br><br><br>", unsafe_allow_html=True)
 
             # Actualizar el texto de la última actualización
             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            last_update_text.markdown(f"<h4 style='text-align: center;'>Last update: {current_time}</h4>", unsafe_allow_html=True)
+            st.markdown("")
+            st.markdown("")
+            last_update_text.markdown(f"<h6 style='text-align: center;'>Last update: {current_time}</h6>", unsafe_allow_html=True)
 
-            # Esperar el tiempo de actualización
+
+            st.markdown("<p style='text-align: left;'>Participate with the QR below:</p>", unsafe_allow_html=True)
+
+            if qr_image_path.exists():
+                st.image(str(qr_image_path), width=100)#use_column_width=True)
+            else:
+                st.error(f"Error: La imagen '{qr_image_path}' no se encuentra.")
+
             time.sleep(refresh_interval)
         else:
             st.warning("No se pudo cargar ningún dato. Revisa la conexión.")
